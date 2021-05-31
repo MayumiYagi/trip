c7ad40d63f16ce341758910ebb9e3b4ac66992f7
MayumiYagi <snow.030m@gmail.com>
2021-05-31 14:31:11 +0900

    追加ソースコード

---
+++ trip/js/base.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('#slick01').slick({
+    arrows: true,
+    prevArrow: '<div class="slide-arrow prev-arrow"><img src="./img/arrow-prev.png"></div>',
+    nextArrow: '<div class="slide-arrow next-arrow"><img src="./img/arrow-next.png"></div>'
+  });
+});
